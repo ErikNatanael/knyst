0df1af22527c52ea26bf68097d15b3c2022c6cce
Erik Natanael Gustafsson <erik@eriknatanael.com>
2022-09-01 15:11:19 +0200

    Bring JACK example sounds into CPAL example

--- examples/cpal_example.rs
+++ examples/cpal_example.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use knyst::{
     audio_backend::{CpalBackend, CpalBackendOptions},
+    graph::Mult,
     prelude::*,
     wavetable::{Wavetable, WavetableOscillatorOwned},
 };
@@ -14,16 +15,29 @@ fn main() {
     let mut graph: Graph = Graph::new(GraphSettings {
         block_size,
         sample_rate,
-        latency: Duration::from_millis(0),
+        latency: Duration::from_millis(100),
         num_outputs: backend.num_outputs(),
         ..Default::default()
     });
     backend.start_processing(&mut graph, resources).unwrap();
     let node0 = graph.push_gen(WavetableOscillatorOwned::new(Wavetable::sine()));
-    graph.connect(Connection::out(node0)).unwrap();
-    graph.connect(Connection::out(node0).to_index(1)).unwrap();
     graph.connect(constl(440., "freq").to_node(node0)).unwrap();
+    let modulator = graph.push_gen(WavetableOscillatorOwned::new(Wavetable::sine()));
+    graph
+        .connect(constl(5., "freq").to_node(modulator))
+        .unwrap();
+    let mod_amp = graph.push_gen(Mult);
+    graph.connect(modulator.to(mod_amp)).unwrap();
+    graph.connect(consti(0.25, 1).to_node(mod_amp)).unwrap();
+    let amp = graph.push_gen(Mult);
+    graph.connect(node0.to(amp)).unwrap();
+    graph.connect(consti(0.5, 1).to_node(amp)).unwrap();
+    graph.connect(mod_amp.to(amp).to_index(1)).unwrap();
+    graph.connect(Connection::out(amp)).unwrap();
+    graph.connect(Connection::out(amp).to_index(1)).unwrap();
     graph.commit_changes();
+    graph.update(); // Required because constant connections get converted to
+                    // scheduled changes when the graph is running.
     let mut input = String::new();
     loop {
         match std::io::stdin().read_line(&mut input) {
