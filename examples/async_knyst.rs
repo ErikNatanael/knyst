f8112957890b20958148d91e2bde9170c38e3388
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-02-22 23:03:58 +0100

    Add background harmonics in a separate thread and graph

--- examples/async_knyst.rs
+++ examples/async_knyst.rs
@@ -2,16 +2,19 @@ use anyhow::Result;
 use knyst::{
     async_api,
     audio_backend::{CpalBackend, CpalBackendOptions},
-    graph::Mult,
+    graph::{Mult, NodeAddress},
     prelude::*,
     wavetable::{Wavetable, WavetableOscillatorOwned},
 };
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use std::io::Write;
 use std::time::Duration;
 
 use termion;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+
+static ROOT_FREQ: f32 = 200.;
 
 fn main() -> Result<()> {
     let mut backend = CpalBackend::new(CpalBackendOptions::default())?;
@@ -35,23 +38,17 @@ fn main() -> Result<()> {
             scheduling_latency: Duration::from_millis(100),
         },
     )?;
+    let num_outputs = backend.num_outputs();
     // We start the asynchronous api on another thread and receive an object
     // with which we can communicate with the graph.
     //
     // TODO: Combine with start_processing or something to ensure we have access
     // to modifying resources as well.
     let mut tk = async_api::start_async_knyst_thread(top_level_graph);
-    // let sub_graph = Graph::new(GraphSettings {
-    //     block_size,
-    //     sample_rate,
-    //     num_outputs: backend.num_outputs(),
-    //     ..Default::default()
-    // });
-    // let sub_graph_id = sub_graph.id();
 
     // Nodes are pushed to the top level graph if no graph id is specified
     let node0 = tk.push(WavetableOscillatorOwned::new(Wavetable::sine()));
-    tk.connect(constant(440.).to(&node0).to_label("freq"));
+    tk.connect(constant(ROOT_FREQ).to(&node0).to_label("freq"));
     let modulator = tk.push(WavetableOscillatorOwned::new(Wavetable::sine()));
     tk.connect(constant(5.).to(&modulator).to_label("freq"));
     let mod_amp = tk.push(Mult);
@@ -59,11 +56,69 @@ fn main() -> Result<()> {
     tk.connect(constant(0.25).to(&mod_amp).to_index(1));
     let amp = tk.push(Mult);
     tk.connect(node0.to(&amp));
-    tk.connect(constant(0.5).to(&amp).to_index(1));
+    tk.connect(constant(0.25).to(&amp).to_index(1));
     tk.connect(mod_amp.to(&node0).to_label("freq"));
     tk.connect(amp.to_graph_out());
     tk.connect(amp.to_graph_out().to_index(1));
 
+    // Have a background thread play some harmony
+    {
+        let chords = vec![
+            vec![0, 17, 31],
+            vec![53, 17, 31],
+            vec![0, 22, 36],
+            vec![0, 22, 39],
+            vec![-5, 9, 31],
+        ];
+        let mut k = tk.clone();
+        std::thread::spawn(move || {
+            let mut rng = thread_rng();
+            let mut sub_graph = Graph::new(GraphSettings {
+                block_size,
+                sample_rate,
+                num_outputs,
+                name: String::from("subgraph"),
+                ..Default::default()
+            });
+
+            let sub_graph_id = sub_graph.id();
+            let sub_graph = k.push(sub_graph);
+            k.connect(sub_graph.to_graph_out().channels(2));
+            let mut harmony_wavetable = Wavetable::sine();
+            harmony_wavetable.add_odd_harmonics(8, 1.3);
+            harmony_wavetable.normalize();
+            let harmony_nodes: Vec<NodeAddress> = (0..chords[0].len())
+                .map(|_| {
+                    let node = k.push_to_graph(
+                        WavetableOscillatorOwned::new(harmony_wavetable.clone()),
+                        sub_graph_id,
+                    );
+                    let amp = k.push_to_graph(Mult, sub_graph_id);
+
+                    k.connect(constant(400.).to(&node).to_label("freq"));
+                    k.connect(node.to(&amp));
+                    k.connect(constant(0.05).to(&amp).to_index(1));
+                    k.connect(amp.to_graph_out());
+                    k.connect(amp.to_graph_out().to_index(1));
+                    node
+                })
+                .collect();
+            loop {
+                let new_chord = chords.choose(&mut rng).unwrap();
+                for (i, node) in harmony_nodes.iter().enumerate() {
+                    k.schedule_change(
+                        ParameterChange::now(
+                            node.clone(),
+                            degree_53_to_hz(new_chord[i] as f32, ROOT_FREQ * 2.0),
+                        )
+                        .label("freq"),
+                    );
+                }
+
+                std::thread::sleep(Duration::from_millis(rng.gen_range(500..2500)));
+            }
+        });
+    }
     // Set terminal to raw mode to allow reading stdin one key at a time
     let mut stdout = std::io::stdout().into_raw_mode().unwrap();
 
@@ -95,7 +150,7 @@ fn main() -> Result<()> {
                     tk.schedule_change(
                         ParameterChange::now(modulator.clone(), new_freq * 5.).l("freq"),
                     );
-                    tk.schedule_change(ParameterChange::now(mod_amp.clone(), new_freq * 2.0).i(1));
+                    tk.schedule_change(ParameterChange::now(mod_amp.clone(), new_freq * 0.1).i(1));
 
                     stdout.lock().flush().unwrap();
                 }
@@ -138,9 +193,16 @@ fn character_to_hz(c: char) -> f32 {
             'h' => 45,
             'u' => 48,
             'j' => 53,
+            'i' => 5 + 53,
             'k' => 62,
+            'o' => 14 + 53,
+            'l' => 17 + 53,
+            'p' => 22 + 53,
+            'ö' => 26 + 53,
+            'å' => 31 + 53,
+            'ä' => 36 + 53,
             _ => 0,
         } as f32,
-        200.,
+        ROOT_FREQ * 4.,
     )
 }
