aa0d1eb9cad5ce80e15d5a30f0effe593cb5d1f1
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-06-06 16:23:12 +0200

    More in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,12 +2,14 @@
 
 pub use crate::audio_backend::AudioBackend;
 pub use crate::buffer::{Buffer, BufferKey, BufferReader};
+pub use crate::controller::{CallbackHandle, KnystCommands};
 pub use crate::graph::{
     connection::constant,
     connection::{ConnectionBundle, InputBundle},
-    gen, Connection, GenContext, GenState, Graph, GraphInput, GraphSettings, Mult, PanMonoToStereo,
-    ParameterChange, Ramp, RunGraphSettings,
+    gen, Connection, GenContext, GenState, Graph, GraphInput, GraphSettings, Mult, NodeAddress,
+    PanMonoToStereo, ParameterChange, Ramp, RunGraphSettings,
 };
 pub use crate::inputs;
+pub use crate::time::{Superbeats, Superseconds};
 pub use crate::wavetable::{Wavetable, WavetableKey, TABLE_POWER, TABLE_SIZE};
 pub use crate::{AnyData, Resources, ResourcesSettings, Sample, StopAction};
